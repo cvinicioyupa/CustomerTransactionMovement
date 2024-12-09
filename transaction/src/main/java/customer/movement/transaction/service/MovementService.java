e95ee903aee294f781b4b41d60564b4bc06c8102
cvinicioyupa <94200315+cvinicioyupa@users.noreply.github.com>
2024-12-09 10:43:49 -0500

    Update Service Movimiento
    Se agrego para que tome el valor inicial de la cuenta en el caso que no tenga registros de movimientos

--- transaction/src/main/java/customer/movement/transaction/service/MovementService.java
+++ transaction/src/main/java/customer/movement/transaction/service/MovementService.java
@@ -26,7 +26,7 @@ public class MovementService {
         this.accountRepository = accountRepository;
     }
 
-  
+ /* 
     public Mono<Movement> create(Movement movement) {
         log.info("Attempting to create movement for account number: {}", movement.getAccountNumber());
 
@@ -73,6 +73,52 @@ public class MovementService {
                 Mono.error(new IllegalArgumentException("Account does not exist with number: " + movement.getAccountNumber()))
             );
     }
+*/
+
+public Mono<Movement> create(Movement movement) {
+    log.info("Creacion del movimiento: {}", movement.getAccountNumber());
+
+    if (movement.getValue() <= 0) {
+        log.error("El monto tiene que ser mayor a 0.");
+        return Mono.error(new IllegalArgumentException("El monto tiene que ser mayor a 0"));
+    }
+
+    return Mono.justOrEmpty(accountRepository.findById(movement.getAccountNumber()))
+        .flatMap(account -> {
+            return Flux.fromIterable(movementRepository.findAll())
+                .filter(m -> m.getAccountNumber() == movement.getAccountNumber())
+                .collectList()
+                .flatMap(movements -> {
+                    // Si no existe el movimientos previo traigo el saldo inicial de la cuenta
+                    double newBalance = movements.stream()
+                        .max(Comparator.comparing(Movement::getId))
+                        .map(Movement::getBalance)
+                        .orElse(account.getInitialBalance());
+
+                    if ("debito".equalsIgnoreCase(movement.getType())) {
+                        if (newBalance < movement.getValue()) {
+                            log.error("La cuenta no tiene el saldo suficiente: {}", movement.getAccountNumber());
+                            return Mono.<Movement>error(new IllegalArgumentException("Saldo no disponible"));
+                        }
+                        newBalance -= movement.getValue();
+                    } else if ("credito".equalsIgnoreCase(movement.getType())) {
+                        newBalance += movement.getValue();
+                    }
+
+                    movement.setBalance(newBalance);
+                    movement.setDate(java.sql.Timestamp.valueOf(java.time.LocalDateTime.now()));
+                    account.setInitialBalance(newBalance);
+                    accountRepository.save(account);
+                    log.info("El movimiento se registro con exito: {}", movement.getAccountNumber());
+                    return Mono.justOrEmpty(movementRepository.save(movement));
+                });
+        })
+        .switchIfEmpty(
+            Mono.error(new IllegalArgumentException("La cuenta no existe: " + movement.getAccountNumber()))
+        );
+}
+
+
 
     public Mono<Movement> getById(int id) {
         return Mono.justOrEmpty(movementRepository.findById(id));
